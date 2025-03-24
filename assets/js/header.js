3f3d63b3c4000d3a370e1f2a840b96381cb7de9f
Rapuluchukwu <adrapulu@gmail.com>
2025-03-24 21:00:36 +0100

    made my header, about page, blogs and footer responsive

--- assets/js/header.js
+++ assets/js/header.js
@@ -75,8 +75,9 @@ document.write(`
             <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="currentColor" viewBox="0 0 24 24">
               <path d="M0 0h24v24H0z" fill="none"/>
               <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-            </svg>
-          </a>
+              </svg>
+              <span>Close</span>
+              </a>
         </div>
       </div>
       <div class="sidebar-content">
